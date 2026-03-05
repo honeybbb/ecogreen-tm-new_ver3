63e3d4d9f9ec03b8c19652ce66413359b8516bad
danbi <danbi681591@gmail.com>
2026-03-05 18:01:00 +0900

    03/05 - 템플릿 정리

---
+++ nuxt.config.ts
@@ -0,0 +1,42 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: false },
+  devServer: {
+      port: 3000,
+  },
+    modules: ['@pinia/nuxt'],
+    css: [
+        // '/assets/css/common.css',
+        '/assets/css/common.css',
+    ],
+    plugins: [
+        '~/plugins/axios.js'
+    ],
+    app: {
+        pageTransition: { name: 'page', mode: 'out-in' },
+        head: {
+            title: '에코그린티엠(ver3)',
+            meta: [
+                { name: 'description', content: '에코그린티엠(ver3) ERP 시스템' }
+            ],
+            link: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css'
+                }
+            ]
+        }
+    },
+    vite: {
+        server: {
+            proxy: {
+                "/api": {
+                    target: 'http://localhost:3001',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''), // 이제 에러 안 납니다!
+                }
+            }
+        }
+    }
+})
